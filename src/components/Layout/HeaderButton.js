ad5b6c6866208ef318342321052ab3a19b960827
usulemre <83465642+usulemre@users.noreply.github.com>
2022-01-07 00:08:37 +0300

    Meals Component

--- src/components/Layout/HeaderButton.js
+++ src/components/Layout/HeaderButton.js
@@ -1,13 +1,15 @@
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderButton.module.css'
-const HeaderButton = () =>{
-    return<button className={classes.button}>
-        <span className={classes.ıcon}>
-            <CartIcon />
-        </span>
-        <span className={classes.cart}>Your Cart</span>
-        <span className={classes.badge}>3</span>
+import classes from "./HeaderButton.module.css";
+const HeaderButton = () => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.ıcon}>
+        <CartIcon />
+      </span>
+      <span className={classes.cart}>Your Cart</span>
+      <span className={classes.badge}>3</span>
     </button>
-}
+  );
+};
 
 export default HeaderButton;
